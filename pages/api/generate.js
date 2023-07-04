a6583de88047e09f5db9ce2cffdad6622abb55b1
stoody-dev <forakshay.dev@gmail.com>
2023-07-04 23:01:26 +0530

    /

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,18 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);({})
+const opencv = require('opencv4nodejs');
+const cv = require('opencv4nodejs');
+const fs = require('fs');
+const path = require('path');
+
+const { create, all } = require('mathjs');
+const math = create(all);
+
+
+
+
 
 const basePromptPrefix = "";
 const generateAction = async (req, res) => {
@@ -20,6 +31,21 @@ const generateAction = async (req, res) => {
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
+  $(document).ready(function() {
+    $.ajax({
+      url: 'https://www.troyhunt.com/authentication-and-the-have-i-been-pwned-api/',
+      type: 'GET',
+      dataType: 'json',
+      success: function() { alert('hello!'); },
+      error: function() { alert('boo!'); }
+      ,
+      headers: {
+  'User-Agent': 'uaheader'
+}
+    });
+  });
+
+  
 
   res.status(200).json({ output: basePromptOutput });
   const basePromptOutputText = basePromptOutput.text;({
@@ -29,7 +55,7 @@ const generateAction = async (req, res) => {
     max_tokens: 250,
   });
   res.status(200).json({ output: basePromptOutput });
-  loadDefaultErrorComponents
+  
   
   
   
